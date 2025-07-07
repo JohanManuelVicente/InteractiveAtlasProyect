8cf16c11d1df611e8feb55442822f9c9a3c5a0a1
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-07-07 07:44:04 -0400

    Makin Users.cs entitie

---
+++ InteractiveAtlas/InteractiveAtlas/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace InteractiveAtlas.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Column("UserId")]
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = null!;
+
+        public string? email { get; set; }
+
+        public string Password { get; set; } = null!;
+
+        public bool IsAdmin { get; set; }  = false;
+
+    }
+}
