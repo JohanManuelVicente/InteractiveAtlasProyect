3f604a73e7ae2a7716ad1838cd52a07fb518b18b
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-07-07 07:32:34 -0400

    Making QuizQuetion entitie

---
+++ InteractiveAtlas/InteractiveAtlas/Entities/QuizQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace InteractiveAtlas.Entities
+{
+    [Table("QuizQuestions")]
+    public class QuizQuestion
+    {
+        [Column("QuestionId")]
+        public int Id { get; set; }
+
+        public string text { get; set; } = null!;
+
+        public string? ProvinceId { get; set; }
+
+        public string? DifficultyLevel { get; set; }
+
+
+    }
+}
