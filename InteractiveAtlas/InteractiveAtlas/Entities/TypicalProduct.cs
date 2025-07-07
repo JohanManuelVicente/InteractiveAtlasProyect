9012844cc47f936123c617f1ede2b68fce774e9f
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-07-07 07:28:19 -0400

    Creaitnf TipycalProducts enntie

---
+++ InteractiveAtlas/InteractiveAtlas/Entities/TypicalProduct.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace InteractiveAtlas.Entities
+{
+    [Table("TypicalProducts")]
+    public class TypicalProduct
+    {
+        [Column("ProductId")]
+        public int Id { get; set; }
+
+        public int ProvinceId { get; set; }
+
+        public string Name { get; set; } = null!; 
+
+        public string? description { get; set; }
+
+        public string? imageUrl { get; set; }
+
+
+    }
+}
