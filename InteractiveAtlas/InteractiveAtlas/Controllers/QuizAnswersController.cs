10b38cc64c1972a487682ad17679a8849ad8cadc
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-07-19 22:56:03 -0400

    Finishing the controllers

---
+++ InteractiveAtlas/InteractiveAtlas/Controllers/QuizAnswersController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InteractiveAtlas.Entities;
+using System.Xml.Linq;
+using InteractiveAtlas.Data;
+using InteractiveAtlas.DTOs;
+
+namespace InteractiveAtlas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuizAnswersController : ControllerBase
+    {
+        private readonly InteractiveAtlasDbContext _context;
+
+        public QuizAnswersController(InteractiveAtlasDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetQuizAnswers()
+        {
+            var quizAnswers = _context.QuizAnswers.ToList();
+            return Ok(quizAnswers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetQuizAnswerById(int id)
+        {
+            var quizAnswer = _context.QuizAnswers.FirstOrDefault(a => a.Id == id);
+
+            if (quizAnswer == null)
+            {
+                return BadRequest($"Quiz Answer with ID: {id} not found");
+            }
+
+            return Ok(quizAnswer);
+        }
+
+        [HttpPost]
+        public IActionResult CreateQuizAnswer([FromBody] QuizAnswerDto request)
+        {
+            if (request == null)
+            {
+                return BadRequest("The QuizAnswer cannot be null");
+            }
+
+            var questionExists = _context.QuizQuestions.Any(q => q.Id == request.QuestionId);
+            if (!questionExists)
+            {
+                return BadRequest($"La pregunta con ID {request.QuestionId} no existe");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Text))
+            {
+                return BadRequest("El texto de la respuesta es requerido");
+            }
+
+            var quizAnswer = new QuizAnswer
+            {
+                Text = request.Text,
+                IsCorrect = request.IsCorrect,
+                QuestionId = request.QuestionId,
+            };
+
+            _context.QuizAnswers.Add(quizAnswer);
+            _context.SaveChanges();
+            return Ok(new { id = quizAnswer.Id });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateQuizAnswer(int id, [FromBody] QuizAnswerDto request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest("El ID de la URL no coincide con el ID de la peticion");
+            }
+
+            if (request.Text == null)
+            {
+                return BadRequest("El texto de la respuesta es nulo");
+            }
+
+            var existingQuizAnswer = _context.QuizAnswers.FirstOrDefault(qa => qa.Id == request.Id);
+            if (existingQuizAnswer == null)
+            {
+                return NotFound($"La respuesta con ID {request.Id} no fue encontrada");
+            }
+
+            var questionExists = _context.QuizQuestions.Any(q => q.Id == request.QuestionId);
+            if (!questionExists)
+            {
+                return BadRequest($"La pregunta con ID {request.QuestionId} no existe");
+            }
+
+            existingQuizAnswer.Text = request.Text;
+            existingQuizAnswer.IsCorrect = request.IsCorrect;
+            existingQuizAnswer.QuestionId = request.QuestionId;
+
+            _context.QuizAnswers.Update(existingQuizAnswer);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteQuizAnswer(int id)
+        {
+            var quizAnswer = _context.QuizAnswers.FirstOrDefault(a => a.Id == id);
+            if (quizAnswer == null)
+            {
+                return NotFound($"QuizAnswer con ID: {id} no fue encontrada");
+            }
+
+            _context.QuizAnswers.Remove(quizAnswer);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ InteractiveAtlas/InteractiveAtlas/Controllers/QuizQuestionsController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InteractiveAtlas.Entities;
+using System.Xml.Linq;
+using InteractiveAtlas.Data;
+using InteractiveAtlas.DTOs;
+
+namespace InteractiveAtlas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuizQuestionsController : ControllerBase
+    {
+        private readonly InteractiveAtlasDbContext _context;
+
+        public QuizQuestionsController(InteractiveAtlasDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetQuizQuestions()
+        {
+            var quizQuestions = _context.QuizQuestions.ToList();
+            return Ok(quizQuestions);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetQuizQuestionById(int id)
+        {
+            var quizQuestion = _context.QuizQuestions.FirstOrDefault(q => q.Id == id);
+
+            if (quizQuestion == null)
+            {
+                return BadRequest($"Quiz Question with ID: {id} not found");
+            }
+
+            return Ok(quizQuestion);
+        }
+
+        [HttpPost]
+        public IActionResult CreateQuizQuestion([FromBody] QuizQuestionDto request)
+        {
+            if (request == null)
+            {
+                return BadRequest("The QuizQuestion cannot be null");
+            }
+
+            if (request.ProvinceId.HasValue)
+            {
+                var provinceExists = _context.Provinces.Any(p => p.Id == request.ProvinceId.Value);
+                if (!provinceExists)
+                {
+                    return BadRequest($"La provincia con ID {request.ProvinceId} no existe");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Text))
+            {
+                return BadRequest("El texto de la pregunta es requerido");
+            }
+
+            var quizQuestion = new QuizQuestion
+            {
+                Text = request.Text,
+                DifficultyLevel = request.DifficultyLevel,
+                ProvinceId = request.ProvinceId,
+            };
+
+            _context.QuizQuestions.Add(quizQuestion);
+            _context.SaveChanges();
+            return Ok(new { id = quizQuestion.Id });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateQuizQuestion(int id, [FromBody] QuizQuestionDto request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest("El ID de la URL no coincide con el ID de la peticion");
+            }
+
+            if (request.Text == null)
+            {
+                return BadRequest("El texto de la pregunta es nulo");
+            }
+
+            var existingQuizQuestion = _context.QuizQuestions.FirstOrDefault(qq => qq.Id == request.Id);
+            if (existingQuizQuestion == null)
+            {
+                return NotFound($"La pregunta con ID {request.Id} no fue encontrada");
+            }
+
+            if (request.ProvinceId.HasValue)
+            {
+                var provinceExists = _context.Provinces.Any(p => p.Id == request.ProvinceId.Value);
+                if (!provinceExists)
+                {
+                    return BadRequest($"La provincia con ID {request.ProvinceId} no existe");
+                }
+            }
+
+            existingQuizQuestion.Text = request.Text;
+            existingQuizQuestion.DifficultyLevel = request.DifficultyLevel;
+            existingQuizQuestion.ProvinceId = request.ProvinceId;
+
+            _context.QuizQuestions.Update(existingQuizQuestion);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteQuizQuestion(int id)
+        {
+            var quizQuestion = _context.QuizQuestions.FirstOrDefault(q => q.Id == id);
+            if (quizQuestion == null)
+            {
+                return NotFound($"QuizQuestion con ID: {id} no fue encontrada");
+            }
+
+            _context.QuizQuestions.Remove(quizQuestion);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
